3b7d5eecee2031d83b5c708768c2d807722398e8
letruxux <mrgianfranco483@gmail.com>
2026-06-01 15:11:09 +0200

    feat: shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Outfit } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+const outfit = Outfit({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Wally",
@@ -11,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={cn("font-sans", outfit.variable)}>
+      <body>
+        <TooltipProvider>{children}</TooltipProvider>
+      </body>
     </html>
   );
 }
