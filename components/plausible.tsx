ec18e045981e81347fe2b95b3ebe7866e4bdd294
letruxux <mrgianfranco483@gmail.com>
2026-06-05 17:42:42 +0200

    fix: plausible ssr

--- components/plausible.tsx
+++ components/plausible.tsx
@@ -1,22 +1,28 @@
 "use client";
 
 import { useEffect } from "react";
-import { init } from "@plausible-analytics/tracker";
+
+async function setup() {
+  if (typeof window === "undefined") return;
+
+  const { init } = await import("@plausible-analytics/tracker");
+  const domain = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN;
+  if (!domain) return;
+
+  const host = process.env.NEXT_PUBLIC_PLAUSIBLE_HOST;
+
+  init({
+    domain,
+    endpoint: host ? `https://${host}/api/event` : undefined,
+    autoCapturePageviews: true,
+    outboundLinks: true,
+    fileDownloads: true,
+  });
+}
 
 export function Plausible() {
   useEffect(() => {
-    const domain = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN;
-    if (!domain) return;
-
-    const host = process.env.NEXT_PUBLIC_PLAUSIBLE_HOST;
-
-    init({
-      domain,
-      endpoint: host ? `https://${host}/api/event` : undefined,
-      autoCapturePageviews: true,
-      outboundLinks: true,
-      fileDownloads: true,
-    });
+    setup();
   }, []);
 
   return null;
